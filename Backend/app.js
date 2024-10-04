3c1259aca59f54ec8bf03307640c55048c00e823
dishagarg3521 <168576259+dishagarg3521@users.noreply.github.com>
2024-10-04 22:12:09 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
  const cookieParser = require("cookie-parser");
 // const bodyParser = require("body-parser");
-// const fileUpload = require("express-fileupload");
+ const fileUpload = require("express-fileupload");
 // const path = require("path");
+const dotenv = require("dotenv");
 
  const errorMiddleware = require("./middleware/error");
 
@@ -11,22 +12,23 @@ const app = express();
 // if (process.env.NODE_ENV !== "PRODUCTION") {
 //   require("dotenv").config({ path: "backend/config/config.env" });
 // }
+dotenv.config({ path: "backend/config/config.env" });
 
  app.use(express.json());
  app.use(cookieParser());
 // app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(fileUpload());
+ app.use(fileUpload());
 
 // // Route Imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
  const order = require("./routes/orderRoute");
-// const payment = require("./routes/paymentRoute");
+ const payment = require("./routes/paymentRoute");
 
  app.use("/api/v1", product);
  app.use("/api/v1", user);
 app.use("/api/v1", order);
-// app.use("/api/v1", payment);
+ app.use("/api/v1", payment);
 
 // app.use(express.static(path.join(__dirname, "../frontend/build")));
 
